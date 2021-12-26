44380ff20e51638d5b871bc021f10e350526f740
zaid1304 <zaidkhanraz99@gmail.com>
2021-12-26 15:47:54 +0530

    resolving error

--- src/App.js
+++ src/App.js
@@ -5,17 +5,6 @@ import "./App.css";
 import Navbar from "./Component/Navbar.js";
 
 function App() {
-  let menu = document.querySelector("#menu-btn");
-  let navbar = document.querySelector(".header .navbar");
-
-  menu.onclick = () => {
-    menu.classList.toggle("fa-times");
-    navbar.classList.toggle("active");
-  };
-  window.onscroll = () => {
-    menu.classList.remove("fa-times");
-    navbar.classList.remove("active");
-  };
   return (
     <>
       <Navbar />
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 export default function navbar() {
- 
-
   return (
     <div>
       <header className="header">
